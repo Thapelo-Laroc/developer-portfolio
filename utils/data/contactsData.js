22dbcc4d77744bf72b441054e97e1d9eeae93e60
Thapelo-Laroc <thapelo.laroc@gmail.com>
2024-04-02 19:44:48 +0200

    changed Thapelo to Mr and cell number

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'Mr.laroc@gmail.com',
-    phone: '+27 71 553 8227',
+    phone: '+27 72 422 6185',
     address: 'Mbombela City, Mpumalanga Province, South Africa ',
     github: 'https://github.com/Mr-Laroc',
     facebook: 'https://www.facebook.com/profile.php?id=1000123473091&mibextid=ZbWKwL',
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,7 @@ export const personalData = {
   designation: "Software Developer",
   description: "I am Web Developer enthusist specialising in Laravel framework, JavaScript, Server Management, Network Management, Software Development, App Development, Information Technology (Laptop and Cellphone repair, Linux and Windows OS specialist, Network Configurations), Coding Course creator.",
   email: 'Mr.laroc@gmail.com',
-  phone: '+27 71 553 8227',
+  phone: '+27 72 422 6185',
   address: 'Mbombela City, Mpumalanga Province, South Africa ',
   github: 'https://github.com/Mr-Laroc',
   facebook: 'https://www.facebook.com/profile.php?id=1000123473091&mibextid=ZbWKwL',
